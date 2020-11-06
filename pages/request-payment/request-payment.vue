12a1a075388a8919242a8480bbbc762e30fc3a29
tucq <492676375@qq.com>
2020-11-06 13:16:58 +0800

    支付功能开发

--- pages/request-payment/request-payment.vue
+++ pages/request-payment/request-payment.vue
@@ -6,18 +6,16 @@
 				<view class="uni-hello-text uni-center">支付金额</text></view>
 				<view class="uni-h1 uni-center uni-common-mt">
 					<text class="rmbLogo">￥</text>
-					<input class="price" type="digit" :value="price" maxlength="4" @input="priceChange" />
+					<input class="price" type="digit" :value="price" @input="priceChange" />
 				</view>
 			</view>
 			<view class="uni-btn-v uni-common-mt">
 				<!-- #ifdef MP-WEIXIN || MP-QQ -->
 				<button type="primary" @click="weixinPay" :loading="loading">微信支付</button>
 				<!-- #endif -->
-				<!-- #ifdef APP-PLUS -->
-				<template v-if="providerList.length > 0">
+				<!-- <template v-if="providerList.length > 0">
 					<button v-for="(item,index) in providerList" :key="index" @click="requestPayment(item,index)" :loading="item.loading">{{item.name}}支付</button>
-				</template>
-				<!-- #endif -->
+				</template> -->
 			</view>
 		</view>
 	</view>
@@ -29,16 +27,14 @@
 			return {
 				title: 'request-payment',
 				loading: false,
-				price: 1.00,
+				price: 0.01,
 				providerList: []
 			}
 		},
 		onLoad: function() {
-			// #ifdef APP-PLUS
 			uni.getProvider({
 				service: "payment",
 				success: (e) => {
-					console.log("payment success:" + JSON.stringify(e));
 					let providerList = [];
 					e.provider.map((value) => {
 						switch (value) {
@@ -66,19 +62,31 @@
 					console.log("获取支付通道失败：", e);
 				}
 			});
-			// #endif
 		},
 		methods: {
 			weixinPay() {
-				console.log("发起支付");
 				this.loading = true;
+				let that = this;
+				let WX_TOKEN = '';
+				try {
+					const token = uni.getStorageSync('WX_TOKEN');
+					if (token) {
+						WX_TOKEN = token;
+					}
+				} catch (e) {}
 				uni.login({
 					success: (e) => {
-						console.log("login success", e);
 						uni.request({
-							url: `https://unidemo.dcloud.net.cn/payment/wx/mp?code=${e.code}&amount=${this.price}`,
+							url: that.$baseUrl + '/pay/wxPay',
+							data: {
+								totalPrice: that.price,
+								orderNum: "202011050006",
+							},
+							header: {
+								'X-Access-Token': WX_TOKEN,
+							},
 							success: (res) => {
-								console.log("pay request success", res);
+								this.loading = false;
 								if (res.statusCode !== 200) {
 									uni.showModal({
 										content: "支付失败，请重试！",
@@ -86,24 +94,22 @@
 									});
 									return;
 								}
-								if (res.data.ret === 0) {
-									console.log("得到接口prepay_id", res.data.payment);
-									let paymentData = res.data.payment;
+								if (res.data.success) {
+									let paymentData = res.data.result;
 									uni.requestPayment({
 										timeStamp: paymentData.timeStamp,
 										nonceStr: paymentData.nonceStr,
 										package: paymentData.package,
-										signType: 'MD5',
+										signType: paymentData.signType,
 										paySign: paymentData.paySign,
 										success: (res) => {
 											uni.showToast({
-												title: "感谢您的赞助!"
+												title: "感谢您的测试!"
 											})
 										},
 										fail: (res) => {
 											uni.showModal({
-												content: "支付失败,原因为: " + res
-													.errMsg,
+												content: "支付失败,原因为: " + res.errMsg,
 												showCancel: false
 											})
 										},
@@ -113,13 +119,13 @@
 									})
 								} else {
 									uni.showModal({
-										content: res.data.desc,
+										content: res.data.message,
 										showCancel: false
 									})
 								}
 							},
 							fail: (e) => {
-								console.log("fail", e);
+								console.log("fail-1", e);
 								this.loading = false;
 								uni.showModal({
 									content: "支付失败,原因为: " + e.errMsg,
@@ -129,7 +135,7 @@
 						})
 					},
 					fail: (e) => {
-						console.log("fail", e);
+						console.log("fail-2", e);
 						this.loading = false;
 						uni.showModal({
 							content: "支付失败,原因为: " + e.errMsg,
@@ -138,57 +144,59 @@
 					}
 				})
 			},
-			async requestPayment(e, index) {
-				this.providerList[index].loading = true;
-				let orderInfo = await this.getOrderInfo(e.id);
-				console.log("得到订单信息", orderInfo);
-				if (orderInfo.statusCode !== 200) {
-					console.log("获得订单信息失败", orderInfo);
-					uni.showModal({
-						content: "获得订单信息失败",
-						showCancel: false
-					})
-					return;
-				}
-				uni.requestPayment({
-					provider: e.id,
-					orderInfo: orderInfo.data,
-					success: (e) => {
-						console.log("success", e);
-						uni.showToast({
-							title: "感谢您的赞助!"
-						})
-					},
-					fail: (e) => {
-						console.log("fail", e);
-						uni.showModal({
-							content: "支付失败,原因为: " + e.errMsg,
-							showCancel: false
-						})
-					},
-					complete: () => {
-						this.providerList[index].loading = false;
-					}
-				})
-			},
-			getOrderInfo(e) {
-				let appid = "";
-				// #ifdef APP-PLUS
-				appid = plus.runtime.appid;
-				// #endif
-				let url = 'https://demo.dcloud.net.cn/payment/?payid=' + e + '&appid=' + appid + '&total=' + this.price;
-				return new Promise((res) => {
-					uni.request({
-						url: url,
-						success: (result) => {
-							res(result);
-						},
-						fail: (e) => {
-							res(e);
-						}
-					})
-				})
-			},
+			// async requestPayment(e, index) {
+			// 	console.log(JSON.stringify(e),index);
+			// 	this.providerList[index].loading = true;
+			// 	let orderInfo = await this.getOrderInfo(e.id);
+			// 	console.log("得到订单信息", orderInfo);
+			// 	if (orderInfo.statusCode !== 200) {
+			// 		console.log("获得订单信息失败", orderInfo);
+			// 		uni.showModal({
+			// 			content: "获得订单信息失败",
+			// 			showCancel: false
+			// 		})
+			// 		return;
+			// 	}
+			// 	uni.requestPayment({
+			// 		provider: e.id,
+			// 		orderInfo: orderInfo.data,
+			// 		success: (e) => {
+			// 			console.log("success", e);
+			// 			uni.showToast({
+			// 				title: "感谢您的赞助!"
+			// 			})
+			// 		},
+			// 		fail: (e) => {
+			// 			console.log("fail", e);
+			// 			uni.showModal({
+			// 				content: "支付失败,原因为: " + e.errMsg,
+			// 				showCancel: false
+			// 			})
+			// 		},
+			// 		complete: () => {
+			// 			this.providerList[index].loading = false;
+			// 		}
+			// 	})
+			// },
+			// getOrderInfo(e) {
+			// 	let appid = "";
+			// 	// #ifdef APP-PLUS
+			// 	appid = plus.runtime.appid;
+			// 	// #endif
+			// 	console.log("appid="+appid);
+			// 	let url = 'https://demo.dcloud.net.cn/payment/?payid=' + e + '&appid=' + appid + '&total=' + this.price;
+			// 	return new Promise((res) => {
+			// 		uni.request({
+			// 			url: url,
+			// 			success: (result) => {
+			// 				res(result);
+			// 			},
+			// 			fail: (e) => {
+			// 				res(e);
+			// 			}
+			// 		})
+			// 	})
+			// },
 			priceChange(e) {
 				console.log(e.detail.value)
 				this.price = e.detail.value;
@@ -203,7 +211,7 @@
 	}
 
 	button {
-		background-color: #007aff;
+		background-color: #00aa00;
 		color: #ffffff;
 	}
 
@@ -216,9 +224,10 @@
 
 	.price {
 		border-bottom: 1px solid #eee;
-		width: 200rpx;
-		height: 80rpx;
-		padding-bottom: 4rpx;
+		width: 150rpx;
+		/* height: 50rpx; */
+		padding-left: 15rpx;
+		/* padding-bottom: 4rpx; */
 	}
 
 	.ipaPayBtn {
